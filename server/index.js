b0590a1c147faac09746873ce2fb65668727c8b9
jyotiraditya <jyotiraditya24@gmail.com>
2023-06-06 18:47:16 +0530

    chat routes set up done

---
+++ server/index.js
@@ -0,0 +1,24 @@
+import express from "express";
+import cors from "cors";
+import chatRoute from "./routes/chat.js";
+import http from "http";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+const server = http.createServer(app);
+
+app.use("/chat", chatRoute);
+
+
+/* MONGO DB CONNECTION */
+mongoose.connect(process.env.MONGO_URL).then(() => {
+  server.listen(3001, () => {
+    console.log("server running on 3001");
+  });
+});
